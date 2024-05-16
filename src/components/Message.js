6b4ee746b7054e2dba92bd0be954e06b6923ac4d
Ben <kimaniben14@gmail.com>
2024-05-16 20:11:11 +0100

    updates

--- src/components/Message.js
+++ src/components/Message.js
@@ -8,12 +8,12 @@ const Message = () => {
         <div class="contact-info">
             <div class="contact-item">
                 <h3>Email:</h3>
-                <p><a href="mailto:youremail@example.com">salescoffee@greenlife.co.ke</a></p>
-                <p><a href="mailto:youremail@example.com">kimaniben14@gmail.com</a></p>
+                {/* <p><a href="mailto:youremail@example.com">salescoffee@greenlife.co.ke</a></p> */}
+                <p className='blinking-link'><a href="mailto:youremail@example.com">kimaniben14@gmail.com</a></p>
             </div>
             <div class="contact-item">
                 <h3>Phone:</h3>
-                <a href="tel:+254707811992"><i class="fas fa-phone"></i> +254707811992</a>
+                <a className='blinking-link' href="tel:+254707811992"><i class="fas fa-phone"></i> +254707811992</a>
 
                
             </div>
