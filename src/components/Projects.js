04acf25ebf48eb41a9a2384aa08ccca09a156466
Ben <kimaniben14@gmail.com>
2024-05-16 16:04:29 +0100

    added blinking to the live links

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,18 +14,18 @@ const Projects = () => {
         <div className="project-item">
           <h3>Greenlife Bot-site</h3>
           <p>The platform offers information on curated remedies tailored for maximum impact on your farm.</p>
-          <h4><a href="https://green-bot-site.vercel.app/">Live link</a></h4>
+          <h4 className='blinking-link'><a href="https://green-bot-site.vercel.app/">Live link</a></h4>
         </div>
 
         <div className="project-item">
           <h3>WeatherChecker</h3>
           <p>WeatherChecker is your go-to weather webpage for instant updates and future forecasts. With a simple search, get current weather details like temperature, humidity, and rain rate. Plan ahead with hourly forecasts.</p>
-          <h4><a href="https://weather-app-omega-kohl-49.vercel.app/">Live link</a></h4>
+          <h4 className='blinking-link'><a href="https://weather-app-omega-kohl-49.vercel.app/">Live link</a></h4>
         </div>
         <div className="project-item">
           <h3>Health Mate</h3>
           <p>Health Mate web application is designed to address the increasing health concerns related to obesity. It calculates the user's BMI by taking input of their first name, height (in centimeters), and weight (in kilograms), and then provides personalized health advice and a proposed meal plan based on the BMI category.</p>
-          <h4><a href="https://health-mate-psi.vercel.app/">Live link</a></h4>
+          <h4 className='blinking-link'><a href="https://health-mate-psi.vercel.app/">Live link</a></h4>
         </div>
       </div>
     </section>
