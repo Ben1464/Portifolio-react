2dbba1e0898ab4cb38fbccc9b5d590e2e9c9c1b1
Ben <kimaniben14@gmail.com>
2024-05-16 15:27:25 +0100

    stylled profile picture

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../styles/Home.css';
+import profileImage from '../images/IMG_20231222_174337_886.jpg'; // Correctly import the image
 
 const Home = () => {
   return (
     <section id="home" className="home">
-      <img src= '../images/IMG_20231222_174337_886.jpg' alt="Profile" />
+      <img className='profile' src={profileImage} alt="Profile" /> {/* Use the imported image variable */}
       <h2>Welcome to My Portfolio</h2>
       <p>This is a showcase of my work.</p>
     </section>
