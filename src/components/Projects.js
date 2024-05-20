90ad413281baf4b571e07da83a041c8015a05738
Ben <kimaniben14@gmail.com>
2024-05-20 06:55:50 +0100

    added demo videos to the greenbot project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/Projects.css';
+import video from '../videos/Vid_20240517_183410.mp4';
 
 const Projects = () => {
   return (
@@ -8,15 +9,19 @@ const Projects = () => {
       <div className="project-list">
         <div className="project-item">
           <h3>Coffee Digital</h3>
-          <p>Coffee Digital is a comprehensive resource tailored to provide solutions to the challenges facing coffee farmers across the globe. From pest and disease infestations to nutrient deficiencies. The platform offers targeted remedies curated for maximum effectiveness.Use the search button to search for solutions to all agronomical challenges you face in your coffee farming endevors.</p>
+          <p>Coffee Digital is a comprehensive resource tailored to provide solutions to the challenges facing coffee farmers across the globe. From pest and disease infestations to nutrient deficiencies. The platform offers targeted remedies curated for maximum effectiveness. Use the search button to search for solutions to all agronomical challenges you face in your coffee farming endeavors.</p>
           <h4 className='blinking-link'><a href="https://coffee-digital.vercel.app/">Live link</a></h4>
         </div>
         <div className="project-item">
           <h3>Greenlife Bot-site</h3>
           <p>The platform offers information on curated remedies tailored for maximum impact on your farm.</p>
+          <h4>Demo video</h4>
+          <video controls>
+            <source src={video} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
           <h4 className='blinking-link'><a href="https://green-bot-site.vercel.app/">Live link</a></h4>
         </div>
-
         <div className="project-item">
           <h3>Weather-Checker</h3>
           <p>Weather-Checker is your go-to weather webpage for instant updates and future forecasts. With a simple search, get current weather details like temperature, humidity, and rain rate. Plan ahead with hourly forecasts.</p>
