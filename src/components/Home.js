0e015f57f2eed434ab3e8c396cb54b7663419ea0
Ben <kimaniben14@gmail.com>
2024-05-20 12:21:51 +0100

    updates on welcome message display

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
     <section id="home" className="home">
       <img className='profile' src={profileImage} alt="Profile" /> {/* Use the imported image variable */}
       <div className="home-content">
-        <h2 className='welcome'>Hi!,My name is Benson Mwangi.Welcome to My Portfolio</h2>
+        <h2 className='welcome'>Hi!, My name is Benson Mwangi. Welcome to My Portfolio.</h2>
         {/* <p>This is a showcase of my work.</p> */}
       </div>
     </section>
