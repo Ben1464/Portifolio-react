266bb31e8a9a0228f3e61b817e4f7dd192697cef
Ben <kimaniben14@gmail.com>
2024-05-20 14:34:54 +0100

    added a humberger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Navbar.css';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
-      {/* <h1>My Portfolio</h1> */}
-      <ul>
+      <div className="hamburger" onClick={toggleMenu}>
+        <div className={isOpen ? "bar open" : "bar"}></div>
+        <div className={isOpen ? "bar open" : "bar"}></div>
+        <div className={isOpen ? "bar open" : "bar"}></div>
+      </div>
+      <ul className={isOpen ? "nav-links open" : "nav-links"}>
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#projects">Projects</a></li>
         <li><a href="#contact">Contact</a></li>
-        {/* <li><a href="#message">Message</a></li> */}
       </ul>
     </nav>
   );
-}
+};
 
 export default Navbar;
