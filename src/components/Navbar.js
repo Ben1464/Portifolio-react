225b57e9f7b93e291aaf1cb1c3343f5dfda40efa
Ben <kimaniben14@gmail.com>
2024-05-20 17:54:20 +0100

    updates to check

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,17 +10,17 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-      <div className="hamburger" onClick={toggleMenu}>
-        <div className={isOpen ? "bar open" : "bar"}></div>
-        <div className={isOpen ? "bar open" : "bar"}></div>
-        <div className={isOpen ? "bar open" : "bar"}></div>
-      </div>
       <ul className={isOpen ? "nav-links open" : "nav-links"}>
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#projects">Projects</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
+      <div className="hamburger" onClick={toggleMenu}>
+        <div className={isOpen ? "bar open" : "bar"}></div>
+        <div className={isOpen ? "bar open" : "bar"}></div>
+        <div className={isOpen ? "bar open" : "bar"}></div>
+      </div>
     </nav>
   );
 };
