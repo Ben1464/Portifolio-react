a5ffbb7288cdfd68cc92edf0959f33919aa8555a
Ben <kimaniben14@gmail.com>
2024-05-22 06:50:50 +0100

    updates on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,13 +8,17 @@ const Navbar = () => {
     setIsOpen(!isOpen);
   };
 
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
     <nav className="navbar">
       <ul className={isOpen ? "nav-links open" : "nav-links"}>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#projects">Projects</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li><a href="#home" onClick={closeMenu}>Home</a></li>
+        <li><a href="#about" onClick={closeMenu}>About</a></li>
+        <li><a href="#projects" onClick={closeMenu}>Projects</a></li>
+        <li><a href="#contact" onClick={closeMenu}>Contact</a></li>
       </ul>
       <div className="hamburger" onClick={toggleMenu}>
         <div className={isOpen ? "bar open" : "bar"}></div>
